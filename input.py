3e69e458bf91c2572dc1616b2a2f84871a8cf9c9
mkumar2307 <88241057+mkumar2307@users.noreply.github.com>
2022-09-06 15:48:04 +0200

    Add files via upload

---
+++ input.py
@@ -0,0 +1,9 @@
+Person = input("Enter your Name: ")
+print("Hello ", Person)
+
+x = input("Enter a Number: ")
+y = input("Enter another number: ")
+
+z = print( int(x) + int(y) )
+
+#print(z)
---
+++ stringmethods.py
@@ -0,0 +1,20 @@
+g = "hello world."
+
+print(g.capitalize())
+
+print(g.upper())
+
+print (g.islower())
+
+print(g.find("world"))
+
+print(g.count("l"))
+
+print(g.split())
+
+print("show me\n d money")
+
+print(g + "Hope u r doing ok. ")
+
+#replace method to replace world with people
+print(g.replace("world", "people"))
