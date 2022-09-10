28a8fdcfb1e80b0d7b0dba582c6ccdb76e789415
Manoj Kumar Gudipati <Manoj.KumarGudipati@nexergroup.com>
2022-09-10 17:56:45 +0200

    new file

---
+++ fstring.py
@@ -0,0 +1,18 @@
+name = "bluelime"
+age = 6
+print(f"Hello, {name}. You are {age} years old.")
+
+print(f"{7 + 7}")
+
+print(f"{name.upper()} is 247 online learning. ")
+
+profession = "developer"
+
+message = (f"Hi {name}. "
+            f"you are a {profession} "
+            f"you are working in DevOps for {age} years. ")
+
+print(message)
+
+car = {'brand': 'Range Rover', 'model': 'HSE' }
+print (f" The car i like is {car['brand']} sports {car['model']}. ")
