812dd6de74937c7efb4d1985ac3cfad62f5bbe94
Manoj Kumar Gudipati <Manoj.KumarGudipati@nexergroup.com>
2022-09-10 18:18:30 +0200

    adding new files

---
+++ mylist.py
@@ -0,0 +1,14 @@
+animals = ["bear", "tiger", "lion", "panda", "elephant"]
+
+for x in animals:
+    print(x)
+print(animals)
+
+#accessing elements of list
+print(animals[0])
+print(animals[1:])
+print(animals[1:3])
+
+animals[0] = "dog"
+print(animals)
+print(animals[0])
