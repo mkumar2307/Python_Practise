fb61c6318d38707a425b8ddb556e1135e647954b
Manoj Kumar Gudipati <manojgudipati@hotmail.com>
2023-05-24 13:52:29 +0000

    added comments

--- Max-number-neighboringpairs-evensum.py
+++ Max-number-neighboringpairs-evensum.py
@@ -5,14 +5,20 @@ One element can belong to only one pair. Write a function: def solution(A)
 '''
 
 def neighbouring_pair(q):
-    matches = []
+    matches = []  # Initialize an empty list to store the indices of matching pairs
+
+    # Iterate through the elements of the array except the last one
     for z in range(len(q) - 1):
-        summ = int(q[z]) + int(q[(z + 1)])
+        summ = int(q[z]) + int(q[(z + 1)])  # Calculate the sum of the current element and the next element
+
+        # Check if the sum is even
         if (summ % 2) == 0:
-            if z not in matches:
-                matches.append((z + 1))
-    print(f"Number of matches: {len(matches)}")
-    return len(matches)
+            if z not in matches:  # Check if the current index is not already in the matches list
+                matches.append((z + 1))  # Add the index of the next element to the matches list
+
+    print(f"Number of matches: {len(matches)}")  # Print the number of matches found
+    return len(matches)  # Return the count of matches
+
 
 #input array
 A = [4,2,5,8,7,3,7]
