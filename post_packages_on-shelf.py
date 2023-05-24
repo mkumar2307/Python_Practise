8adad5a068c986fe02c04ac6257e55f240ef9bd4
Manoj Kumar Gudipati <manojgudipati@hotmail.com>
2023-05-24 13:56:30 +0000

    Added comments

--- post_packages_on-shelf.py
+++ post_packages_on-shelf.py
@@ -1,3 +1,11 @@
+'''
+N clients are waiting in line to collect their package at post office. Package arrives at the post office one by one. for simplicity we will number the packages from 1 to N in order in which they arrive. Each client wants to collect a single package, the K-th client in the line (for K from 0 to N-1) wants to collect package number client[K].
+After a package arrives one of the following events happens:   If the first client in the line wants to collect the package they pick it up and leave the line; Otherwise the package is put on the shelf.
+If the first client wants to collect a package from the shelf they will leave the line and collect package. please note only the first client from the line can collect their package. How many packages will be stored on the shelf at the same time?
+write a function : def solution(client)
+which gives an array client returns the maximum number of packages that will be sorted on the shelf at same time.
+'''
+
 def solution(clients):
     packages_on_shelf = set()
     max_packages_on_shelf = 0
